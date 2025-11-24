31567d2b058fd8a4e3fb7fb7583dfaaf11ca07cd
LiroxDeYamon <lumividellir@gmail.com>
2025-11-25 01:57:39 +0500

    oops again slipped localhost in the commit

--- dialects/DialectLoader.js
+++ dialects/DialectLoader.js
@@ -28,8 +28,8 @@ function createSandbox() {
 
 const DIALECTS = {
   MAP: {
-    // "dr_dr": "https://draconicconlang.github.io/APIs/dialects/dr_dr/"
-    "dr_dr": "http://127.0.0.1:5500/dialects/dr_dr/"
+    "dr_dr": "https://draconicconlang.github.io/APIs/dialects/dr_dr/"
+    // "dr_dr": "http://127.0.0.1:5500/dialects/dr_dr/"
   },
 
   async load(id, dictionary = true, experimental = false, deprecated = false) {
